1d1b28e2afa171d05351853daa73449a037374ae
abdul-osman-mecca <39681296+osmaniacattack@users.noreply.github.com>
2022-08-27 23:07:24 -0700

    implement sidebar component

--- app/src/components/SearchBar.jsx
+++ app/src/components/SearchBar.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Paper, IconButton } from "@mui/material";
 import { Search} from "@mui/icons-material";
+import '../index.css'
 
 const SearchBar = () => {
   return (
