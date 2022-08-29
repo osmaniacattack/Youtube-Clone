c392564b69e1e1212a3ea5e398de16fc0a13d779
abdul-osman-mecca <39681296+osmaniacattack@users.noreply.github.com>
2022-08-28 20:22:24 -0700

    implement channel page

--- app/src/components/ChannelDetail.jsx
+++ app/src/components/ChannelDetail.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+import { Videos, ChannelCard } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const ChannelDetail = () => {
-  return (
-    <div>ChannelDetail</div>
-  )
-}
+  const [channelDetail, setchannelDetail] = useState(null);
+  const [videos, setvideos] = useState([]);
+  const { id } = useParams();
 
-export default ChannelDetail
+  console.log(channelDetail, videos);
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
+      setchannelDetail(data?.items[0])
+    );
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then(
+      (data) => setvideos(data?.items)
+    );
+  }, [id]);
+
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        <div
+          style={{
+            background:
+              "linear-gradient(90deg, rgba(65,62,110,1) 0%, rgba(9,70,121,1) 35%, rgba(9,79,129,1) 39%, rgba(7,107,156,1) 52%, rgba(46,156,194,1) 68%, rgba(0,212,255,1) 100%)",
+            zIndex: 10,
+            height: "300px",
+          }}
+        />
+        <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+      </Box>
+      <Box display="flex" p="2">
+        <Box sx={{ mr: { sm: "100px" } }} />
+        <Videos videos={[videos]} />
+      </Box>
+    </Box>
+  );
+};
+
+export default ChannelDetail;
