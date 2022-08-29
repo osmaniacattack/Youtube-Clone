2b26c81bdbe7fd6ac7379e4959cbb50bd6947b5d
abdul-osman-mecca <39681296+osmaniacattack@users.noreply.github.com>
2022-08-28 20:52:32 -0700

    implement video sidebar

--- app/src/components/VideoDetail.jsx
+++ app/src/components/VideoDetail.jsx
@@ -2,21 +2,27 @@ import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import ReactPlayer from "react-player";
 import { Typography, Box, Stack } from "@mui/material";
-import { CheckCircle } from "@mui/icons-material";
-import { Video } from "./";
+import { CheckCircle, SettingsInputSvideoSharp } from "@mui/icons-material";
+import { Videos } from "./";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const VideoDetail = () => {
   const { id } = useParams();
   const [videoDetail, setVideoDetail] = useState(null);
+  const [videos, setVideos] = useState(null);
 
   useEffect(() => {
     fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) =>
       setVideoDetail(data.items[0])
     );
-  }, []);
+
+    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`).then(
+      (data) => setVideos(data?.items)
+    );
+  }, [id]);
 
   if (!videoDetail?.snippet) return "Loading...";
+  if (!videos) return "Loading...";
 
   const {
     snippet: { title, channelId, channelTitle },
@@ -64,6 +70,14 @@ const VideoDetail = () => {
               </Stack>
             </Stack>
           </Box>
+        </Box>
+        <Box
+          px={2}
+          py={{ md: 1, xs: 5 }}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Videos videos={[videos]} direction="column" />
         </Box>
       </Stack>
     </Box>
--- app/src/components/Videos.jsx
+++ app/src/components/Videos.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Stack, Box } from "@mui/material";
 import { VideoCard, ChannelCard } from "./";
 
-const Videos = ({ videos }) => {
+const Videos = ({ videos, direction }) => {
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+    <Stack direction={direction || "row" } flexWrap="wrap" justifyContent="start" gap={2}>
       {videos[0].map((item, idx) => (
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item} />}
